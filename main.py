b27681a90b8f07d7bcab3efa4c5176726820cdca
kabust <tulyulyuk91@gmail.com>
2024-04-16 18:21:52 +0200

    added core project structure

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from city.router import router as city_router
+
+
+app = FastAPI()
+
+app.include_router(city_router)
+
+
+@app.get("/")
+def index():
+    return {"message": "hello world"}

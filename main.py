16df8198e2e30f50bfb1dcaf9f962c37c48ce3d0
kabust <tulyulyuk91@gmail.com>
2024-04-17 00:48:51 +0200

    added endpoints to the index page

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
 
 from city.router import router as city_router
+from temperature.router import router as temperature_router
 
 
 app = FastAPI()
 
 app.include_router(city_router)
+app.include_router(temperature_router)
 
 
 @app.get("/")
 def index():
-    return {"message": "hello world"}
+    return {
+        "Docs": "/docs/",
+        "All cities": app.url_path_for("read_all_cities"),
+        "Add new city": app.url_path_for("create_city"),
+        "All temperature records": app.url_path_for("read_temperature_records"),
+        "Fetch temperature for all cities": app.url_path_for("fetch_temperature_for_all_cities")
+    }
